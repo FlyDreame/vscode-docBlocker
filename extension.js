0d8794345725af6f48457d123bc45dadd497c79c
刘梦6 <liumeng6@hikvision.com>
2017-11-29 19:53:21 +0800

    support Snippet

--- extension.js
+++ extension.js
@@ -2,12 +2,17 @@
  * @Author: liumeng6 
  * @Date: 2017-11-27 19:38:38 
  * @Last Modified by: liumeng6
- * @Last Modified time: 2017-11-29 18:57:49
+ * @Last Modified time: 2017-11-29 19:50:11
  */
 
 const vscode = require('vscode')
 const indentString = require('indent-string')
 
+/** 
+ * @Author: liumeng6 
+ * @Date: 2017-11-29 19:49:22 
+ * @Desc: 格式化时间 
+ */
 Date.prototype.format = function (format) {
     var o = {
         "M+": this.getMonth() + 1, //month
@@ -29,6 +34,11 @@ Date.prototype.format = function (format) {
     return format
 }
 
+/** 
+ * @Author: liumeng6 
+ * @Date: 2017-11-29 19:49:38 
+ * @Desc: es6模版字符串引擎 
+ */
 function stp(strTmpl, data) {
     if (data !== null && data !== undefined) {
       return stp(strTmpl)(data)
@@ -38,6 +48,11 @@ function stp(strTmpl, data) {
     return new Function("$", "with($){return `" + strTmpl + '`;}')
 }
 
+/** 
+ * @Author: liumeng6 
+ * @Date: 2017-11-29 19:49:54 
+ * @Desc: activate函数
+ */
 function activate(context) {
 
     const config = vscode.workspace.getConfiguration('docBlocker')
@@ -48,51 +63,39 @@ function activate(context) {
         const editor = vscode.editor || vscode.window.activeTextEditor
         const startLine = editor.selection.active.line
         // const character = editor.selection.active.character
+        
+        const time = new Date().format("yyyy-MM-dd hh:mm:ss")
+        const tpl = config.tpl
+        const data = {
+            author: config.Author,
+            createTime: time,
+        }
+        try {
+            let textToInsert = stp(tpl, data)
 
-        editor.edit(function (editBuilder) {
-            const time = new Date().format("yyyy-MM-dd hh:mm:ss")
-            const tpl = config.tpl
-            const data = {
-                author: config.Author,
-                createTime: time,
-            }
-            try {
-                let textToInsert = stp(tpl, data)
+            let pos = new vscode.Position(startLine, 0)
 
-                //Check if there is any text on startLine. If there is, add a new line at the end
-                const lastCharIndex = vscode.window.activeTextEditor.document.lineAt(startLine).text.length
-                let pos = null
-                if ((lastCharIndex > 0) && (startLine !=0)) {
-                    pos = new vscode.Position(startLine, lastCharIndex);
-                    // textToInsert = '\n' + textToInsert; 	
+            const line = vscode.window.activeTextEditor.document.lineAt(editor.selection.start.line).text
+            const firstNonWhiteSpace = vscode.window.activeTextEditor.document.lineAt(editor.selection.start.line).firstNonWhitespaceCharacterIndex
+            
+            let stringToIndent = ''
+
+            for (let i = 0; i < firstNonWhiteSpace; i++) {
+                if (line.charAt(i) == '\t') {
+                    stringToIndent = stringToIndent + '\t';
                 }
-                else {
-                    pos = new vscode.Position(startLine, 0);
+                else if (line.charAt(i) == ' ') {
+                    stringToIndent = stringToIndent + ' ';
                 }
-
-                const line = vscode.window.activeTextEditor.document.lineAt(editor.selection.start.line).text
-                const firstNonWhiteSpace = vscode.window.activeTextEditor.document.lineAt(editor.selection.start.line).firstNonWhitespaceCharacterIndex
-                // const numIndent = 0 
-                // const tabSize = vscode.window.activeTextEditor.options.tabSize
-                let stringToIndent = ''
-
-                for (let i = 0; i < firstNonWhiteSpace; i++) {
-                    if (line.charAt(i) == '\t') {
-                        stringToIndent = stringToIndent + '\t';
-                    }
-                    else if (line.charAt(i) == ' ') {
-                        stringToIndent = stringToIndent + ' ';
-                    }
-                }
-
-                textToInsert = indentString(textToInsert, 1, {indent: stringToIndent})
-                editBuilder.insert(pos, textToInsert.slice(stringToIndent.length))
-                
-            } catch (error) {
-                console.error(error)
             }
 
-        })
+            textToInsert = indentString(textToInsert, 1, {indent: stringToIndent})
+
+            editor.insertSnippet(new vscode.SnippetString(textToInsert), pos)
+            
+        } catch (error) {
+            console.error(error)
+        }
         
     })
 
